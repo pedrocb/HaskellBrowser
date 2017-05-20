b174baad0a79bd3b59af0c08e17bdb38b05bd493
pedrocb <pedrocb_c-s@hotmail.com>
2017-05-21 00:40:44 +0100

    create tree first draft

--- src/HTML.hs
+++ src/HTML.hs
@@ -1,7 +1,24 @@
 module HTML where
 
+import Data.List
+import Text.Parsec
+
 data HtmlElement
   = HtmlText String
   | HtmlElement String [HtmlElement]
 
 newtype HTML = HTML [HtmlElement]
+
+showElement' :: Int -> HtmlElement -> String
+showElement' _ (HtmlText text) = "Text: " ++ text ++ "\n"
+showElement' depth (HtmlElement tag childs) = "Tag: " ++ tag ++ "\n" ++ concatMap (showElementWithDepth' (depth+1)) childs
+
+showElementWithDepth' :: Int -> HtmlElement -> String
+showElementWithDepth' depth element = prependStars depth ++ showElement' (depth) element
+
+prependStars :: Int -> String
+prependStars depth = replicate depth '*'
+
+printTree :: Either ParseError [HtmlElement] -> IO ()
+printTree (Right elements) = putStr (concatMap (showElementWithDepth' 1) elements)
+printTree (Left err) = putStr (show err)
--- src/Parser.hs
+++ src/Parser.hs
@@ -1,1 +1,60 @@
 module Parser where
+
+import Text.Parsec
+import HTML
+import qualified Text.Parsec.Token as P
+import Text.Parsec.Language (emptyDef)
+import Text.ParserCombinators.Parsec.Combinator
+
+type Parser = Parsec String ()
+
+tagIdentifier :: Parser String
+tagIdentifier = do
+  id <- many1 letter;
+  return(id);
+
+startTagOptional :: Parser ()
+startTagOptional = do
+  text <- many1 (noneOf ">")
+  return ()
+
+startTag :: Parser String
+startTag = do
+  char '<';
+  name <- tagIdentifier;
+  optional startTagOptional;
+  char '>';
+  return(name);
+
+endTag :: String -> Parser ()
+endTag name = do
+  string "</";
+  name <- tagIdentifier;
+  char '>';
+  return ();
+
+htmlTextP :: Parser HtmlElement
+htmlTextP = do
+  text <- many1 (noneOf "<>")
+  return(HtmlText text)
+
+
+htmlTagP:: Parser HtmlElement
+htmlTagP = do
+  name <- startTag
+  elements <- htmlElementP
+  endTag name
+  return(HtmlElement name elements)
+
+htmlElementP:: Parser [HtmlElement]
+htmlElementP = many (try htmlTagP <|> htmlTextP)
+
+htmlFileP :: Parser [HtmlElement]
+htmlFileP = do
+  tokens <- htmlElementP;
+  eof;
+  return(tokens)
+
+
+parseHtml :: String -> Either ParseError [HtmlElement]
+parseHtml htmlContent = parse htmlFileP "stdin" htmlContent
